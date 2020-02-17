960c13c1156e04efbf4e340e88056358544f08c1
PetrovNikolia <kintep84@mail.ru>
2020-02-17 17:05:57 +0500

    lesson1

---
+++ desktop/src/my/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package my.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import my.game.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
