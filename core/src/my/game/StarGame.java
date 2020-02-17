99ed35c855834a1acca92e758b7947c3760ba715
PetrovNikolia <kintep84@mail.ru>
2020-02-17 17:59:12 +0500

    start

--- core/src/my/game/StarGame.java
+++ core/src/my/game/StarGame.java
@@ -13,7 +13,7 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("voda.jpg");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -22,7 +22,6 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-
 		batch.end();
 	}
 	
